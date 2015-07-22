e563d9d669695c1ea4d3aa3b7214724b425ebad2
Scott Mathis <sdmathis89@gmail.com>
2015-07-22 16:49:41 -0400

    ChangeSeed Nodes

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -101,7 +101,7 @@ const unsigned THREAD_STACK_SIZE                             = 5 * 1024 * 1024;
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  //"your_seed_ip1.com:8080",
+    "45.63.105.15:18125",
   //"your_seed_ip2.com:8080",
 };
 
